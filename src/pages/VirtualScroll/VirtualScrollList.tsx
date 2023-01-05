587ab292173c5d5b42b86648605804a9c99f4bd0
gevorg <gevoryenokyan@gmail.com>
2023-01-05 23:06:07 +0400

    add virtoual scroll functionality

--- src/pages/VirtualScroll/VirtualScrollList.tsx
+++ src/pages/VirtualScroll/VirtualScrollList.tsx
@@ -1,37 +1,80 @@
-import { FC } from "react";
-import VirtualScroll from "./components/VirtualScroll";
+import { FC, useState, useEffect } from "react";
+import UiVirtualScroll from "./components/VirtualScroll";
 
-const itemCount = 1000;
+type ItemType = string;
 
-interface ScrollListType {
-  id: number;
-  name: string;
-}
+const VirtualScrollList: FC = (): JSX.Element => {
+  const limit = 10;
 
-const scrollList: ScrollListType[] = [];
-for (let i = 0; i < itemCount; i++) {
-  scrollList.push({
-    id: i,
-    name: `Item ${i}`,
-  });
-}
+  const buffer = limit * 3;
 
-const VirtualScrollList: FC = () => {
-  const renderItem = (index: number) => {
-    const item = scrollList[index];
-    return (
-      <div key={item.id} style={{ color: "red", fontSize: "25px" }}>
-        {item.name}
-      </div>
-    );
+  const cache = buffer - limit;
+  const [items, setItems] = useState<ItemType[]>([]);
+  const [isLoading, setIsLoading] = useState<Boolean>(false);
+
+  const callApi = (offset: number, limit: number): Promise<ItemType[]> => {
+    return new Promise((resolve) => {
+      const items = [] as ItemType[];
+      for (let index = offset; index < offset + limit; index++) {
+        items.push("label " + index);
+      }
+
+      setTimeout(() => {
+        resolve(items);
+      }, 2000);
+    });
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    callApi(0, buffer).then((res: ItemType[]) => {
+      setItems(res);
+      setIsLoading(false);
+    });
+  }, []);
+
+  const prevCallback = (newOffset: number): Promise<boolean> => {
+    setIsLoading(true);
+
+    return callApi(newOffset, limit).then((res: ItemType[]) => {
+      const newItems = [...res, ...items.slice(0, cache)] as ItemType[];
+      setItems(newItems);
+      setIsLoading(false);
+      return true;
+    });
+  };
+
+  const nextCallback = (newOffset: number): Promise<boolean> => {
+    setIsLoading(true);
+
+    return callApi(newOffset, limit).then((res: ItemType[]) => {
+      const newItems = [...items.slice(-cache), ...res] as ItemType[];
+      setItems(newItems);
+      setIsLoading(false);
+      return true;
+    });
   };
 
   return (
-    <VirtualScroll
-      itemCount={itemCount}
-      renderItem={renderItem}
-      itemHeight={50}
-    />
+    <div>
+      <UiVirtualScroll
+        buffer={buffer}
+        rowHeight={39}
+        height="50vh"
+        limit={limit}
+        onPrevCallback={prevCallback}
+        onNextCallback={nextCallback}
+        offset={0}
+      >
+        <>
+          {items.map((item: ItemType, index: number) => (
+            <div style={{ padding: "10px", color: "black" }}>
+              {isLoading ? <>Loading...</> : item}
+            </div>
+          ))}
+        </>
+      </UiVirtualScroll>
+    </div>
   );
 };
 
--- src/pages/VirtualScroll/components/VirtualScroll.tsx
+++ src/pages/VirtualScroll/components/VirtualScroll.tsx
@@ -1,48 +1,89 @@
 import { useRef, useEffect, useState, FC } from "react";
 
-interface VirtualScrollProps {
-  itemCount: number;
-  renderItem: (index: number) => React.ReactNode;
-  itemHeight: number;
+export interface UiVirtualScrollProps<T> {
+  offset: 0;
+  buffer: number;
+  limit: number;
+  rowHeight: number;
+  height: string;
+  onPrevCallback: (data: T) => T;
+  onNextCallback: (data: T) => T;
+  children: JSX.Element;
 }
 
-const VirtualScroll: FC<VirtualScrollProps> = ({
-  itemCount,
-  renderItem,
-  itemHeight,
+const UiVirtualScroll: FC<UiVirtualScrollProps<any>> = ({
+  offset,
+  buffer,
+  limit,
+  rowHeight,
+  height,
+  onPrevCallback,
+  onNextCallback,
+  children,
 }) => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const [scrollTop, setScrollTop] = useState(0);
+  const overlayRef: any = useRef<any>(null);
 
-  const handleScroll = () => {
-    if (containerRef.current) {
-      setScrollTop(containerRef.current.scrollTop);
-    }
-  };
+  const [upperBoundary, setUpperBoundary] = useState<number>(offset);
 
-  const renderItems = () => {
-    const items: React.ReactNode[] = [];
-    const startIndex = Math.floor(scrollTop / itemHeight);
-    const endIndex = Math.min(startIndex + 20, itemCount);
-    for (let i = startIndex; i < endIndex; i++) {
-      items.push(renderItem(i));
+  const [lowerBoundary, setLowerBoundary] = useState<number>(buffer - 1);
+  const [isLoading, setIsLoading] = useState<Boolean>(false);
+
+  const [currentScrollTopPosition, setCurrentScrollTopPosition] =
+    useState<number>(0);
+
+  const handleScroll = (target: any) => {
+    if (isLoading) {
+      return;
     }
-    return items;
+
+    const scrollTop = Math.round(target.scrollTop);
+
+    const clientHeight = Math.round(target.clientHeight);
+    const scrollHeight = Math.round(target.scrollHeight);
+
+    const isUp = scrollTop < currentScrollTopPosition;
+
+    if (isUp && scrollTop === 0) {
+      setIsLoading(true);
+
+      onPrevCallback(upperBoundary - limit).then(() => {
+        setUpperBoundary(upperBoundary - limit);
+        setLowerBoundary(lowerBoundary - limit);
+
+        if (overlayRef !== null) {
+          const scrollPos = limit * rowHeight;
+          overlayRef.current.scrollTo(0, scrollPos);
+        }
+        setIsLoading(false);
+      });
+    } else if (!isUp && scrollTop + clientHeight >= scrollHeight) {
+      setIsLoading(true);
+
+      onNextCallback(lowerBoundary).then(() => {
+        setUpperBoundary(upperBoundary + limit);
+        setLowerBoundary(lowerBoundary + limit);
+
+        if (overlayRef !== null) {
+          const scrollPos = limit * rowHeight;
+
+          overlayRef.current.scrollTo(0, scrollPos * 2);
+        }
+        setIsLoading(false);
+      });
+    }
+
+    setCurrentScrollTopPosition(scrollTop);
   };
 
   return (
     <div
-      ref={containerRef}
-      style={{ overflow: "auto", height: "100vh" }}
-      onScroll={() => {
-        handleScroll();
-      }}
+      ref={overlayRef}
+      style={{ height, overflow: "scroll" }}
+      onScroll={(e: any) => handleScroll(e.target)}
     >
-      <div style={{ height: `${itemCount * itemHeight}px` }}>
-        {renderItems()}
-      </div>
+      {children}
     </div>
   );
 };
 
-export default VirtualScroll;
+export default UiVirtualScroll;
